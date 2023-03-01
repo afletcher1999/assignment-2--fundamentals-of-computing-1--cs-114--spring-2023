ea62e214ce561149784b5ad0506b88c87ef80962
afletcher1999 <afletcher@hartford.edu>
2023-03-01 16:11:17 -0500

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -8,7 +8,7 @@ public class Diamond {
     Scanner input = new Scanner(System.in);
 
     // Prompt the user to enter the size of the diamond
-    System.out.print("Enter the size of the diamond: ");
+    System.out.print("Enter the size of the diamond as a whole number: ");
 
     // Read the user input and store it in the 'size' variable
     int size = input.nextInt();
@@ -16,101 +16,84 @@ public class Diamond {
           System.out.println("Invalid size");
           System.exit(0);
         }
-      // Calculate the maximum number of rows in the diamond
-      int maxRows = size ;
 
-      // Calculate the index of the middle row of the diamond
-      int middleRow = maxRows / 2;
+    int maxRows = size ;
+    int middleRow = maxRows / 2;
 
       // If the size of the diamond is even, adjust the maximum number of rows and the index of the middle row
       if (size % 2 == 0) {
          maxRows = size + 3;
          middleRow = (size / 2) + 1; ;
 
-
-               // Loop through each row of the diamond
+        // Loop through each row of the diamond
         for (int row = 0 ; row < maxRows; row++) {
           int spaces = Math.abs(middleRow - row);
 
-
-
-
-
-
-
+          // Inserts the necessary number of spaces at the beginning of the current row
           for (int col = 0; col < spaces; col++) {
-            if( col == middleRow) {
-              System.out.print("  ");
-            }else if(row == 1){
-              System.out.print("  ");
-            }else if(row == maxRows){
+            System.out.print("  ");
+           /*  if( col == middleRow) {
               System.out.print("  ");
             }
-
+            else if(row == 1){
+              System.out.print("  ");
+            }
+            else if(row == maxRows){
+              System.out.print("  ");
+            }
             else{
-              System.out.print( "  ");
-            }
-
+              System.out.print( "  "); */
+            //}
           }
-          if(row == 1) {
-            System.out.print("*");
-            System.out.println();
-          }else if(row == size + 1) {
-            System.out.print("*");
-            System.out.println();
-          }
-          else{
-
-
-          int asterisks = size - spaces * 2;
-
-
-          // Print the necessary number of asterisks in the current row
-          for (int col = 0; col < asterisks * 2; col++) {
-
-
-
-             if(col % 2 == 0 || col == 0) {
-
-              System.out.print(" *");
+              if(row == 1) {
+                // Prints out the first rows single asterisk
+                System.out.print("*");
+                System.out.println();
               }
+              else if(row == size + 1) {
+                // Prints out the last rows single asterisk
+                System.out.print("*");
+                System.out.println();
+              }
+              else{
+                  // Calculate the number of asterisks needed in the current row
+                  int asterisks = size - spaces * 2;
 
-
-          }
-
-          // Move to the next line to start the next row
-          System.out.println();
-
-
-    }
-  }
+                  // Print the necessary number of asterisks in the current row
+                  for (int col = 0; col < asterisks * 2; col++) {
+                    if(col % 2 == 0 || col == 0) {
+                      System.out.print(" *");
+                    }
+                  }
+                  // Move to the next line to start the next row
+                  System.out.println();
+              }
+        }
       }
 
       // Loop through each row of the diamond
-      if(size % 2 != 0){
-        for (int row = 0; row < maxRows; row++) {
-          // Calculate the number of spaces needed before the first asterisk in the current row
-          int spaces = Math.abs(middleRow - row);
+        if(size % 2 != 0){
+          for (int row = 0; row < maxRows; row++) {
+            // Calculate the number of spaces needed before the first asterisk in the current row
+            int spaces = Math.abs(middleRow - row);
 
-          // Print the necessary number of spaces before the first asterisk
-          for (int col = 0; col < spaces; col++) {
+            // Print the necessary number of spaces before the first asterisk
+            for (int col = 0; col < spaces; col++) {
               System.out.print(" ");
-          }
+            }
+            // Calculate the number of asterisks needed in the current row
+            int asterisks = size - spaces * 2;
 
-          // Calculate the number of asterisks needed in the current row
-          int asterisks = size - spaces * 2;
-
-          // Print the necessary number of asterisks in the current row
-          for (int col = 0; col < asterisks; col++) {
+            // Print the necessary number of asterisks in the current row
+            for (int col = 0; col < asterisks; col++) {
               System.out.print("*");
+            }
+            // Move to the next line to start the next row
+            System.out.println();
           }
-
-          // Move to the next line to start the next row
-          System.out.println();
-      }
-    }
-      // Close the Scanner object to prevent resource leak
-      input.close();
+        }
+    // Close the Scanner object to prevent resource leak
+    input.close();
   }
 }
 
